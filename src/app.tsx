a51cbe6551e36ec7dde0c8d3097a7a1054d7c8c5
Jiangda Wang <jiangda_wang@apple.com>
2024-07-15 19:39:47 -0700

    works

--- src/app.tsx
+++ src/app.tsx
@@ -3,26 +3,22 @@ import { Router, action, createAsync, redirect } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Show, Suspense } from "solid-js";
 import "./app.css";
-import { getAuthenticatedUser } from "./lib/auth-utils";
-import { getRequestEvent } from "solid-js/web";
-import { lucia } from "./lib/auth";
-import { appendHeader } from "vinxi/server";
-import { Nav } from "./components/Nav";
+import { Nav } from "~/components/Nav";
 
 export default function App() {
-  return (
-    <Router
-      root={props => {
-        return (
-          <MetaProvider>
-            <Title>SolidStart - Basic</Title>
-            <Nav />
-            <Suspense>{props.children}</Suspense>
-          </MetaProvider>
-        );
-      }}
-    >
-      <FileRoutes />
-    </Router>
-  );
+	return (
+		<Router
+			root={(props) => {
+				return (
+					<MetaProvider>
+						<Title>SolidStart - Basic</Title>
+						<Nav />
+						<Suspense>{props.children}</Suspense>
+					</MetaProvider>
+				);
+			}}
+		>
+			<FileRoutes />
+		</Router>
+	);
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,42 +1,48 @@
 import { action, createAsync, redirect } from "@solidjs/router";
-import { Show } from "solid-js"
+import { Show } from "solid-js";
 import { getRequestEvent } from "solid-js/web";
 import { appendHeader } from "vinxi/server";
 import { lucia } from "~/lib/auth";
 import { getAuthenticatedUser } from "~/lib/auth-utils";
 
-
 export const Nav = () => {
-
-  const user = createAsync(() => getAuthenticatedUser());
-  return (
-    <div class="border-b flex flex-row justify-between items-center">
-      <div>
-            <a href="/">Index</a>
-            <a href="/about">About</a>
-      </div>
-            <Show when={user()} fallback={
-            <button class="border text-zinc-300 bg-black" type="button">
-              Login in with GitHub
-            </button>}>
-              <div class="inline-flex">
-                Hi, {user()?.username}
-                <form method="post" action={logout}>
-                  <button type="submit">Sign out</button>
-                </form>
-              </div>
-            </Show>
-            </div>
-  )
-}
+	const user = createAsync(() => getAuthenticatedUser());
+	return (
+		<div class="border-b flex flex-row justify-between items-center">
+			<div>
+				<a href="/">Index</a>
+				<a href="/about">About</a>
+			</div>
+			<Show
+				when={user()}
+				fallback={
+					<button class="border text-zinc-300 bg-black" type="button">
+						Login in with GitHub
+					</button>
+				}
+			>
+				<div class="inline-flex">
+					Hi, {user()?.username}
+					<form method="post" action={logout}>
+						<button type="submit">Sign out</button>
+					</form>
+				</div>
+			</Show>
+		</div>
+	);
+};
 
 const logout = action(async function logout() {
-  "use server";
+	"use server";
 	const event = getRequestEvent()!;
 	if (!event.locals.session) {
 		return new Error("Unauthorized");
 	}
 	await lucia.invalidateSession(event.locals.session.id);
-	appendHeader(event.nativeEvent, "Set-Cookie", lucia.createBlankSessionCookie().serialize());
+	appendHeader(
+		event.nativeEvent,
+		"Set-Cookie",
+		lucia.createBlankSessionCookie().serialize(),
+	);
 	throw redirect("/login");
-})
+});
