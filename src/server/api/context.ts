b0885389992aaae1d5c222d765364961d9668c19
Frank Wang <1454884738@qq.com>
2024-07-15 22:52:16 -0500

    clean up

--- src/server/api/context.ts
+++ src/server/api/context.ts
@@ -1,11 +1,9 @@
 import type { RequestEvent } from "solid-js/web";
 
 export function createContext(event: RequestEvent) {
-  console.log(JSON.stringify(event.locals))
 	return {
 		user: event.locals.user,
 		session: event.locals.session,
 	};
-
 }
 export type Context = Awaited<ReturnType<typeof createContext>>;
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,15 +1,11 @@
-import { wrap } from "@typeschema/valibot";
-import * as v from 'valibot';
+import * as v from "valibot";
 import { authedProcedure, createTRPCRouter, publicProcedure } from "../utils";
 
 export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(v.parser(v.string()))
-    .query(({ input }) => {
-      return `Hello ${input}!`;
-    }),
-  secret: authedProcedure.query(({ctx}) => {
-    return ctx.user.username
-  })
-  
+	hello: publicProcedure.input(v.parser(v.string())).query(({ input }) => {
+		return `Hello ${input}!`;
+	}),
+	secret: authedProcedure.query(({ ctx }) => {
+		return ctx.user.username;
+	}),
 });
